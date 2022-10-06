7f4fc398ab0fc419ceefed8256996f6bac6035f2
minhpham160603 <minhpham160603@gmai.com>
2022-10-06 12:05:01 +0200

    add new line in main.cpp

--- main.cpp
+++ main.cpp
@@ -36,6 +36,7 @@ int main(int argc, char* argv[])
 
   {
          std::cout << "A better, new initial message" << std::endl;
+         std::cout << "New changes moa ha ha";
   }
   // END OF THE CUSTOM CODE SECTION
 #else
